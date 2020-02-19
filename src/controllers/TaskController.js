028905982f403960f2cdc8238af935b933567fe9
Lourival Cavalcante <cavalcantebsi@gmail.com>
2020-02-19 17:10:08 -0400

    delete and update task was add

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,22 +1,22 @@
-const User = require('../models/User');
-const Tasks = require('../models/Tasks');
+const User = require('../models/User')
+const Tasks = require('../models/Tasks')
 
 module.exports = {
     async index(req, res) {
         const { user } = req.headers;
     
-        const task = await Tasks.find({ user: user });
+        const task = await Tasks.find({ user: user })
     
-        return res.json(task);
+        return res.json(task)
       },
       async store(req, res){
-        const { title, priority, note } = req.body;
-        const { user_id } = req.headers;
+        const { title, priority, note } = req.body
+        const { user_id } = req.headers
 
-        const user = await User.findById(user_id);
+        const user = await User.findById(user_id)
 
         if (!user) {
-        return res.status(400).json({ error: 'User does not exists' });
+        return res.status(400).json({ error: 'User does not exists' })
         }
 
         const task = await Tasks.create({
@@ -27,6 +27,53 @@ module.exports = {
         })
 
         return res.json(task)
+      },
+      async destroy(req, res){
+        const { user_id, task_id} = req.headers
+
+        const user = await User.findById(user_id)
+        const task = await Tasks.findById(task_id)
+
+
+        if (!user) {
+          return res.status(400).json({ error: 'User does not exists' })
+        }
+        if (!task) {
+          return res.status(400).json({ error: 'Task does not exists' })
+        }
+
+        const taskDeleted = await Tasks.deleteOne({
+          user: user_id,
+          _id: task_id
+          },
+          function (err){
+            if(err) console.log(err);
+            return res.json("Successful deletion")
+          })
+
+      },
+      async update(req, res){
+        const { title, priority, note } = req.body
+
+        const { user_id } = req.headers
+
+        const user = await User.findById(user_id)
+
+        if (!user) {
+          return res.status(400).json({ error: 'User does not exists' })
+        }
+
+        const task = await Tasks.updateOne({
+          user: user_id,
+          title,
+          priority,
+          note
+          },
+          function (err){
+            if(err) console.log(err);
+            return res.json(task)
+          })
+
       }
 
     
--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,7 @@ const routes = express.Router()
 routes.post('/sessions', SessionController.store)
 routes.post('/tasks', TaskController.store)
 routes.get('/tasks', TaskController.index)
+routes.delete('/tasks', TaskController.destroy)
+routes.post('/tasksUpdate', TaskController.update)
 
 module.exports = routes;
